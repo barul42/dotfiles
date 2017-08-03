9f8377c98200fc09d3069a35b2165163550b53e5
barul <barul@barul.info>
2017-08-03 19:18:00 +0000

    added scripts

---
+++ scripts/basic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir ~/git
+cd ~/git
+git clone https://github.com/barul42/dotfiles
+mkdir ~/.bash/
+mkdir ~/.vim/
+ln -sf ~/git/dotfiles/bash/bashrc ~/.bashrc
+ln -sf ~/git/dotfiles/bash/functions.sh ~/.bash/functions.sh
+ln -sf ~/git/dotfiles/vim/vimrc ~/.vimrc
+ln -sf ~/git/dotfiles/vim/colors ~/.vim/colors
+source ~/.bashrc
