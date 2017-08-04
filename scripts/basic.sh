82cb0779dbab51116118022ad53fd066f4ba50a5
barul <barul@barul.info>
2017-08-04 17:27:03 +0000

    edited basic.sh

--- scripts/basic.sh
+++ scripts/basic.sh
@@ -4,10 +4,14 @@ cd ~/git
 git clone https://github.com/barul42/dotfiles
 mkdir ~/.bash/
 mkdir ~/.vim/
+mkdir -p ~/.config/i3
+mkdir ~/.config/i3status
 ln -sf ~/git/dotfiles/bash/bashrc ~/.bashrc
 ln -sf ~/git/dotfiles/bash/functions.sh ~/.bash/functions.sh
 ln -sf ~/git/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/git/dotfiles/vim/colors ~/.vim/colors
 ln -sf ~/git/dotfiles/Xresources ~/.Xresources
 ln -sf ~/git/dotfiles/xinitrc ~/.xinitrc
+ln -sf ~/git/dotfiles/i3/config ~/.config/i3/
+ln -sf ~/git/dotfiles/i3/i3status/config ~/.config/i3status/
 source ~/.bashrc
